c168b714307a6235a14f518956e96cfa5ee93172
rakibchowdhury1144@gmail.com <rakibchowdhury1144@gmail.com>
2025-11-29 20:34:44 +0600

    public profiles added

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -2,9 +2,9 @@
         <div class="footer-container">
             <p class="footer-paragraph">&copy; 2025 Rakib Chowdhury. All rights reserved.</p>
             <div class="social-links">
-                <a href="https://www.facebook.com/rakib" target="_blank">Facebook</a>
-                <a href="https://www.linkedin.com/in/rakib" target="_blank">LinkedIn</a>
-                <a href="https://github.com/rakib" target="_blank">GitHub</a>
+                <a href="https://www.facebook.com/kyreerakib" target="_blank">Facebook</a>
+                <a href="https://www.linkedin.com/in/rakib-chowdhury-35b3b4369/" target="_blank">LinkedIn</a>
+                <a href="https://github.com/dev-rakib" target="_blank">GitHub</a>
             </div>
         </div>
 </footer>
